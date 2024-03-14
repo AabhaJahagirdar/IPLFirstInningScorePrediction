e12826fa5e4affd6678dd31192fdb7054dbd6c43
AabhaNJ123 <AabhaJahagirdar@gmail.com>
2024-03-14 12:28:00 +0530

    first commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict():
     temp_array = list()
     
     if request.method == 'POST':
-        
+        pitch=request.form['pitch']
         batting_team = request.form['batting-team']
         if batting_team == 'Chennai Super Kings':
             temp_array = temp_array + [1,0,0,0,0,0,0,0]
@@ -27,6 +27,7 @@ def predict():
             temp_array = temp_array + [0,1,0,0,0,0,0,0]
         elif batting_team == 'Kings XI Punjab':
             temp_array = temp_array + [0,0,1,0,0,0,0,0]
+            
         elif batting_team == 'Kolkata Knight Riders':
             temp_array = temp_array + [0,0,0,1,0,0,0,0]
         elif batting_team == 'Mumbai Indians':
@@ -56,6 +57,10 @@ def predict():
             temp_array = temp_array + [0,0,0,0,0,0,1,0]
         elif bowling_team == 'Sunrisers Hyderabad':
             temp_array = temp_array + [0,0,0,0,0,0,0,1]
+
+        
+        
+            
             
             
         overs = float(request.form['overs'])
@@ -63,11 +68,35 @@ def predict():
         wickets = int(request.form['wickets'])
         runs_in_prev_5 = int(request.form['runs_in_prev_5'])
         wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+
+        if wickets>10 or wickets<0:
+            raise Exception("Invalid Input at wickets")
+        if runs < 0 :
+            raise Exception("Negative input given at runs which is not considered")
+        if runs_in_prev_5>runs or runs_in_prev_5<0:
+            raise Exception("Inavlid Input at runs in previous 5 over input")
+        if wickets_in_prev_5>wickets:
+            raise Exception("Invalid Input at wickets in previous 5 over input")
         
         temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
         
         data = np.array([temp_array])
         my_prediction = int(regressor.predict(data)[0])
+
+        if pitch == 'greenpitch':
+            my_prediction-=6
+        elif pitch == 'deadpitch':
+            my_prediction+=2
+        elif pitch == 'dustypitch':
+            my_prediction-=2
+        elif pitch == 'wetpitch':
+            my_prediction-=4
+        elif pitch == 'drypitch':
+            my_prediction+=2
+        elif pitch == 'flattrackpitch':
+            my_prediction+=8
+        else :
+            raise Exception("Something went wrong")
               
         return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
 
